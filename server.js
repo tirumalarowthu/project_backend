fd6b57ffea806edc645e63d6c4d04e17143eda12
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-02-28 16:08:55 +0530

    cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv").config()
+const cors=require("cors")
+
 const chats = require("./data")
+app.use(cors())
 app.get("/", (req, res) => {
     res.send("This is backend related to the chat application")
 })
@@ -12,6 +15,7 @@ app.get("/api/chat/:id", (req, res) => {
     const singlechat = chats.find(item => item._id === req.params.id)
     res.send(singlechat)
 })
+
 
 
 
