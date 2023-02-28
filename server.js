13efbcfcd9718dbe28325bb19152749e266f6f10
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-02-28 14:20:02 +0530

    vercel added

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express=require("express")
 const app=express()
+app.get("/",(req,res)=>{
+    res.send("This is backend related to the chat application")
+})
+
+
+
+
+
+
 app.listen(5050,()=>{
     console.log(`app listening at port 5000`)
 })  
