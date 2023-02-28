3677925a0054fb83bdce1093a9521d37d30c6638
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-02-28 15:50:39 +0530

    chats

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express=require("express")
-const app=express()
-app.get("/",(req,res)=>{
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv").config()
+const chats = require("./data")
+app.get("/", (req, res) => {
     res.send("This is backend related to the chat application")
+})
+app.get("/api/chat", (req, res) => {
+    res.send(chats)
+})
+app.get("/api/chat/:id", (req, res) => {
+    const singlechat = chats.find(item => item._id === req.params.id)
+    res.send(singlechat)
 })
 
 
 
 
 
-
-app.listen(5050,()=>{
-    console.log(`app listening at port 5000`)
+app.listen(process.env.PORT, () => {
+    console.log(`app listening at port ${process.env.PORT}`)
 })  
